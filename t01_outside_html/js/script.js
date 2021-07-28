4fd17e8edf1570b0faa8be1b6c02e97be86f6df0
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-07-28 08:01:32 +0300

    first commit

---
+++ t01_outside_html/js/script.js
@@ -0,0 +1,4 @@
+const str = "Hello JavaScript from outside!"
+alert(str);
+/*f;fef dv.v.fd
+fdrfedfdfefesffff*/
---
+++ t04_solver/js/script.js
@@ -0,0 +1,9 @@
+let x = solver()
+
+function solver(a, b, c, d, e) {
+    return x = a ** 2 - 5 * b * c + d / 3 + e
+    alert(x)
+
+}
+
+solver(5, 6, 3, -5, 2)
