3820b85df7524630c32d90463ca7b4ab7c3943db
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-07-30 23:56:28 +0300

    ff

--- t09_namemaker/js/script.js
+++ t09_namemaker/js/script.js
@@ -1,50 +1,41 @@
+function input() {
+    const animal = prompt("What animal is the superhero most similar to?", "")
 
-
-
-function animal_input() {
-    let animal = prompt("What animal is the superhero most similar to?", "" )
-
-    if (animal.length <= !20 && animal.match(!/[a - z]/s / gi)) {
-       return "success"
-    
+    if (animal.length > 20 || animal.match(/[^a-z]/gi)) {
+        return "fail"
     }
-    else {
-        return fail
+    const age = prompt("How old is the superhero?")
+    if (age.length > 5 || age.match(/\D/gi)) {
+        return "fail"
+
+    }
+    const gender = prompt("Is the superhero male or female? Leave blank if unknown or other", "male, female or blank")
+
+    if (gender === "male" && Number(age) < 18) {
+        return `The superhero name is: ${animal}- boy!`
+
+    }
+
+    if (gender === "male" && Number(age) >= 18) {
+        return `The superhero name is: ${animal}- man!`
+    }
+
+    if (gender === "female" && Number(age) >= 18) {
+        return `The superhero name is: ${animal}- woman!`
+    }
+    if (gender === "female" && Number(age) < 18) {
+        return `The superhero name is: ${animal}- girl!`
+    }
+
+    if (gender.trim() === "" && Number(age) < 18) {
+        return `The superhero name is: ${animal}- kid!`
+    }
+    if (gender.trim() === "" && Number(age) >= 18) {
+        return `The superhero name is: ${animal}- hero!`
+    } else {
+        return "fail"
     }
 
 }
 
-function gender(){
-    let gender = prompt("Is the superhero male or female? Leave blank if unknown or other", "male, female or blank")
-    if (gender == "male"|| gender == "female"|| gender == ""){
-        return "success!"
-    }
-    else{
-        return "Fail"
-    }
-  
-}
-
-function age_input(){
-    let animal = prompt("What animal is the superhero most similar to?", "")
-    let gender = prompt("Is the superhero male or female? Leave blank if unknown or other", "male, female or blank")
-    let age = prompt("How old is the superhero?")
-    let des_1
-    let des_2
-    let des_3
-    let des_4
-    let des_5
-    switch (ani) {
-        case "male":
-            des_1 = `The superhero name is:${animal}-man!`
-            
-            break;
-    
-        default:
-            break;
-    }
-  
-}
-
-alert(age_input())
-
+alert(input())
