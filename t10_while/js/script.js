a3b5a2387cf4e77b6efd7e07e9f167588389e6be
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-08-01 00:23:21 +0300

    solve loop and string

--- t10_while/js/script.js
+++ t10_while/js/script.js
@@ -1,18 +1,17 @@
-let number = 2
-let i = 0
-let res1 = ''
+let number = 2;
+let i = 0;
+let res1 = "";
+
 do {
-    i = i + 1;
-    res1 += "2 *    " + i + " = " + i * 2 + "\n";
+  i = i + 1;
+  res1 += "2 * " + i + " = " + i * 2 + "\n";
+} while (i < 9);
 
-
-}
-while (i < 9)
-i = 9
-let res2 = ""
+i = 9;
+let res2 = "";
 while (i <= 9) {
-    res2 += i * 2 + ""
-    i++
+  res2 += i * 2 + "";
+  i++;
 }
 
-alert(res1)
+alert(res1);
--- t13_greeting/js/script.js
+++ t13_greeting/js/script.js
@@ -1,23 +1,23 @@
-function greet() {
-    const first_name = prompt("Please, enter your first name", "").toUpperCase(1)
-        if(first_name.match(/[^a-z]/gmi)){
-        return "Wrong input!"
-    }
-
-    
-    const last_name = prompt("Please, enter your first name", "").toUpperCase(1)
-    if(last_name.match(/[^a-z]/gmi)){
-        return "Wrong input!"
-    }
-    
-
-    
-    else
-    {
-        return `Hello,${first_name}  ${last_name}`
-    }
+function capital_first_character(name) { // senya
+    const first_character = name.charAt(0).toUpperCase() // s
+    return first_character + name.slice(1) //S + enya
 }
-    
+
+function greet() {
+    const pattern = /[^a-z]/gmi
+
+    const first_name = prompt("Please, enter your first name", "")
+    if (first_name.match(pattern)) {
+        return "Wrong input!"
+    }
+
+    const last_name = prompt("Please, enter your first name", "")
+    if (last_name.match(pattern)) {
+        return "Wrong input!"
+    }
+    return `Hello, ${capital_first_character(first_name)} ${capital_first_character(last_name)}`
+}
+
 
 alert(greet())
 console.log(greet())
