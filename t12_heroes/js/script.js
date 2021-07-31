668dba43e3626275c43e492f24c9355a9559c36e
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-07-31 14:02:23 +0300

    ds

---
+++ t12_heroes/js/script.js
@@ -0,0 +1,16 @@
+const arr = [ 'hero1', 'hero2', 'hero3', 'hero4', 'hero5', 'hero6' ]
+function heros(arr, i) {
+    if(i<=0){
+        return arr1 = []
+    }
+  return arr1= arr.slice(0, i)
+
+}
+
+console.log(heros(arr,-1))
+console.log(heros(arr, 6))
+console.log(heros(arr, 7))
+console.log(heros(arr,-4))
+console.log(heros(arr,3))
+console.log(heros(arr,-2))
+console.log(heros(arr, 2))
