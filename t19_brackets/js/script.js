3b272e018d5481033e2d177e58d7014e959ac93a
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-08-09 13:57:14 +0300

    t19

--- t19_brackets/js/script.js
+++ t19_brackets/js/script.js
@@ -1,24 +1,23 @@
 
 
- function gr(str){
-     if(str.match(/[(]|[)]/g) ==-1||str.split("").indexOf("(") ==-1){
-         return - 1
-     }
-    
+function gr(str) {
+    if (str.indexOf(")") == -1 && str.indexOf("(") == -1) {
+        return - 1
+    }
     let str2 = str.match(/[(]|[)]/g).join("")
     let left = 0
-    let right =0
-     for(let i = 0 ; i< str2.length; i++){
-         left+= str2[i] =="("?1:-1
-         if(left ==-1){
-             left+=1
-             right+=1
-         }
-     }
+    let right = 0
+    for (let i = 0; i < str2.length; i++) {
+        left += str2[i] == "(" ? 1 : -1
+        if (left == -1) {
+            left += 1//((
+            right += 1
+        }
+    }
     return left + right
- }
- console.log(gr("FE"));
-  
+}
+console.log(gr("FE"));
+
 
 
 
