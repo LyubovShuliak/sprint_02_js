a189a0e1131a9f713938b1a5f00c022de9ff98ed
LyubovShuliak <87687676+LyubovShuliak@users.noreply.github.com>
2021-08-09 09:44:58 +0300

    task 19

--- t19_brackets/js/script.js
+++ t19_brackets/js/script.js
@@ -1,35 +1,28 @@
-let str = "))()(F)()(((FE"
-const found = str.match(/[()]/g)
-const found_Right = str.match(/[)]/g)
-const found_Left = str.match(/[()]/g)
 
-let found2 = found.toString().replace(/[,]/g, "").trim()
-let lenght = found2.length
-console.log(found2)
-console.log(lenght)
 
-let i 
-let res1 = ""
-let res2 = []
-for (i = found2.indexOf("("); i < found.length;  i+= found2.indexOf(")") ){
-    res2.push(res1)
-    res1 = found2.indexOf(")", i) - found2.indexOf("(",i) 
+
+ 
+
+ function gr(p){
     
-   }
-function isOdd(value) {
-    if(value>0 && value%2==1){
-        return value
-    }       
-}
+    let str2 = p.match(/[(]|[)]/g).join("")
+    
+    let left = 0
+    let right =0
+     for(let i = 0 ; i< str2.length; i++){
+         left+= str2[i] =="("?1:-1
+         if(left ==-1){
+             left+=1
+             right+=1
 
-let filtered = res2.filter(isOdd)
-console.log(filtered)
-function checkBrackets() {
-
-    return lenght - filtered.length * 2
+         }
+     }
+    return left + right
+ }
+ console.log(gr("))()(F)()(((FE"));
   
-}
-console.log(checkBrackets())
+
+
 
 
 
